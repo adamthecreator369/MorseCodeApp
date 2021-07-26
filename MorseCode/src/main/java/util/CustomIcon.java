33b52e24e3f47ba110d2f043fc03871b995ca0ae
nm811 <77762086+nm811@users.noreply.github.com>
2021-07-26 11:09:47 -0500

    Add comments to code

--- MorseCode/src/main/java/util/CustomIcon.java
+++ MorseCode/src/main/java/util/CustomIcon.java
@@ -1,3 +1,5 @@
+/* Created by Neha Metlapalli on 07/25/2021 */
+
 package main.java.util;
 
 import java.awt.Color;
@@ -10,26 +12,37 @@ import main.java.resources.R;
 
 public class CustomIcon implements Icon {
 	Color color;
+	
+	// Default constructor
     public CustomIcon() {
 	    this(Color.WHITE);
     }
+    
+    // Constructor
     public CustomIcon(Color c) {
     	color = c;
     }
+    
+    @Override
     public void paintIcon(Component c, Graphics g, int x, int y) {
-    	g.setColor(color);
-    	g.fillOval(x, y, getIconWidth(), getIconHeight());
-    	g.setColor(R.color.DARK_GRAY);
+    	g.setColor(color); // Set the color of the radio button icon
+    	g.fillOval(x, y, getIconWidth(), getIconHeight()); // Now fill an oval with that color
+    	// Change the color now to dark gray and create a border around the radio button icon
+    	g.setColor(R.color.DARK_GRAY); 
     	g.drawOval(x, y, getIconWidth(), getIconHeight());
     }
+    
+    // Getters
     public int getIconWidth() {
     	return 10;
     }
+    
     public int getIconHeight() {
     	return 10;
     }
+    
+    // Setter
     public void setColor(Color c) {
     	color = c;
     }
-    
 }
