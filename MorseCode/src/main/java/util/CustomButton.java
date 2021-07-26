b337feab2a575a7bab96779aa1b09b4b9ce294b5
nm811 <77762086+nm811@users.noreply.github.com>
2021-07-26 11:07:44 -0500

    Add comments to code

--- MorseCode/src/main/java/util/CustomButton.java
+++ MorseCode/src/main/java/util/CustomButton.java
@@ -1,3 +1,5 @@
+/* Created by Neha Metlapalli on 07/25/2021 */
+
 package main.java.util;
 
 import java.awt.Color;
@@ -14,42 +16,46 @@ public class CustomButton extends JButton {
 	private Color hoverBackgroundColor;
     private Color pressedBackgroundColor;
 
+    // Default constructor
     public CustomButton() {
     	this(null);
     }
 
+    // Constructor
     public CustomButton(String text) {
         super(text);
         super.setContentAreaFilled(false);
         setBackground(R.color.WHITE);
         setHoverBackgroundColor(Color.decode("#f5f7f6"));
         setPressedBackgroundColor(Color.decode("#ededed"));
-        setBorder(new LineBorder(R.color.WHITE));
+        setBorder(new LineBorder(R.color.DARK_GRAY));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-    	if (getModel().isPressed()) {
-    		g.setColor(pressedBackgroundColor);
-        } else if (getModel().isRollover()) {
-            g.setColor(hoverBackgroundColor);
-        } else {
-            g.setColor(getBackground());
+    	if (getModel().isPressed()) { // If the button is pressed
+    		g.setColor(pressedBackgroundColor); // Set the background color of the button to pressedBackgroundColor
+        } else if (getModel().isRollover()) { // Else if the mouse if over the button
+            g.setColor(hoverBackgroundColor); // Set the background color of the button to hoverBackgroundColor
+        } else { // Else if the mouse is not on the button
+            g.setColor(getBackground()); // Set the background color of the button to the default background color
         }
-        g.fillRect(0, 0, getWidth(), getHeight());
-        super.paintComponent(g);
+        g.fillRect(0, 0, getWidth(), getHeight()); // Now create the background rectangle of the button
+        super.paintComponent(g); 
     }
 
+    // Getters
     public Color getHoverBackgroundColor() {
     	return hoverBackgroundColor;
     }
-
-    public void setHoverBackgroundColor(Color hoverBackgroundColor) {
-        this.hoverBackgroundColor = hoverBackgroundColor;
-    }
-
+    
     public Color getPressedBackgroundColor() {
         return pressedBackgroundColor;
+    }
+
+    // Setters
+    public void setHoverBackgroundColor(Color hoverBackgroundColor) {
+        this.hoverBackgroundColor = hoverBackgroundColor;
     }
 
     public void setPressedBackgroundColor(Color pressedBackgroundColor) {
